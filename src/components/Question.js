27401db88d65ebbcf0c9c48c871019faf5fe3416
+ <+>
2023-08-22 12:03:43 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Create a timer that runs every 1 second
+    const timer = setInterval(() => {
+      // Decrease timeRemaining by 1
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    // Cleanup function for useEffect
+    return () => {
+      clearInterval(timer); // Clear the timer when the component unmounts
+    };
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      // Reset timeRemaining to 10 seconds
+      setTimeRemaining(10);
+      // Call the onAnswered callback with a value of false
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   const { id, prompt, answers, correctIndex } = question;
 
@@ -19,7 +34,7 @@ function Question({ question, onAnswered }) {
       {answers.map((answer, index) => {
         const isCorrect = index === correctIndex;
         return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+          <button key={answer} onClick={() => onAnswered(isCorrect)}>
             {answer}
           </button>
         );
